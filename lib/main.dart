d812fcc3eb87181a329599b3426534a378267c0c
Arun Kumar <ak299730p@gmail.com>
2024-01-12 22:37:15 +0530

    basic skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,185 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() { 
+runApp(const MyApp()); 
+} 
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget { 
+const MyApp({Key? key}) : super(key: key); 
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+@override 
+Widget build(BuildContext context) { 
+	return MaterialApp( 
+	title: 'Travel Companion!',
+	debugShowCheckedModeBanner: false, 
+	home: const HomePage(), 
+	); 
+} 
+} 
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+class HomePage extends StatefulWidget { 
+const HomePage({Key? key}) : super(key: key); 
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+@override 
+_HomePageState createState() => _HomePageState(); 
+} 
 
-  final String title;
+class _HomePageState extends State<HomePage> { 
+int pageIndex = 0; 
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+final pages = [ 
+	const Homepage(), 
+	const Requests(), 
+	const Search(), 
+	const Profile(), 
+]; 
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+@override 
+Widget build(BuildContext context) { 
+	return Scaffold( 
+	backgroundColor: const Color(0xffC4DFCB), 
+	appBar: AppBar(
+    backgroundColor: Colors.grey[800],
+        title: Row(
+          children: [
+            Image.asset(
+              'lib/assets/images/logo.png',
+              fit: BoxFit.contain,
+              height: 50.0,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Container(
+              padding: EdgeInsets.all(8.0),
+              child: Text('  Travel Companion'),
+              
+            )
           ],
         ),
+    ),
+	body: pages[pageIndex], 
+	bottomNavigationBar: buildMyNavBar(context), 
+	); 
+} 
+
+Container buildMyNavBar(BuildContext context) { 
+	return Container(
+        color: Colors.black,
+         child: Padding(
+           padding: EdgeInsets.symmetric(
+            horizontal: 15.0,
+            vertical: 20.0,
+           ),
+           child: GNav(
+            gap: 8.0,
+            padding: EdgeInsets.all(16),
+            backgroundColor: Colors.black,
+            color: Colors.white,
+            activeColor: Colors.white,
+            tabBackgroundColor: Colors.grey,
+
+            tabs: [
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: Icons.message,
+                text: 'Requests',
+                ),
+              GButton(
+                icon: Icons.search,
+                text: 'Search',
+                ),
+              GButton(
+                icon: Icons.person,
+                text: 'Profile',
+                ),
+            ],
+            selectedIndex: pageIndex,
+            onTabChange: (index){
+              setState(() {
+                pageIndex = index;
+              });
+            },
+           ),
+         )
+      ); 
+} 
+} 
+
+class Homepage extends StatelessWidget { 
+const Homepage({Key? key}) : super(key: key); 
+
+@override 
+Widget build(BuildContext context) { 
+	return Container( 
+	color: Color.fromARGB(255, 236, 14, 14), 
+	child: Center( 
+		child: Text( 
+		"Homepage",
+    style: TextStyle(
+      fontSize: 35.0,
+    ),
+		), 
+		), 
+	); 
+} 
+} 
+
+class Requests extends StatelessWidget { 
+const Requests({Key? key}) : super(key: key); 
+
+@override 
+Widget build(BuildContext context) { 
+	return Container( 
+	color: Color.fromARGB(255, 216, 78, 181), 
+	child: Center( 
+		child: Text(
+      "Requests",
+      style: TextStyle(
+        fontSize: 35.0,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
+      ) 
+	), 
+	); 
+} 
+} 
+
+class Search extends StatelessWidget { 
+const Search({Key? key}) : super(key: key); 
+
+@override 
+Widget build(BuildContext context) { 
+	return Container( 
+	color: Color.fromARGB(255, 238, 126, 21), 
+	child: Center( 
+		child: Text(
+      "Search",
+      style: TextStyle(
+        fontSize: 35.0,
+      ),
+    ), 
+	), 
+	); 
+} 
+} 
+
+class Profile extends StatelessWidget { 
+const Profile({Key? key}) : super(key: key); 
+
+@override 
+Widget build(BuildContext context) { 
+	return Container( 
+	color: Color.fromARGB(255, 10, 117, 160), 
+	child: Center( 
+		child: Text(
+      "Profile",
+      style: TextStyle(
+        fontSize: 35.0,
+      ),
+    ), 
+	), 
+	); 
+} 
+} 
