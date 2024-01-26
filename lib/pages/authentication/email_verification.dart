1bde1e035c6e06b1caf00d5d9172632cf8335345
Anadi Sharma <sharma.130@iitj.ac.in>
2024-01-27 02:06:55 +0530

    Login+Signup+VerifyPage ui added

---
+++ lib/pages/authentication/email_verification.dart
@@ -0,0 +1,240 @@
+import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
+import 'package:travel_companiion/pages/authentication/login.dart';
+import 'package:travel_companiion/pages/authentication/signup.dart';
+
+class VerifyPage extends StatefulWidget {
+  const VerifyPage({super.key});
+
+  @override
+  State<VerifyPage> createState() => _VerifyPageState();
+}
+
+class _VerifyPageState extends State<VerifyPage> {
+  var state = 0;
+  var emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 251, 248, 239),
+        body: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.all(15),
+            height: MediaQuery.of(context).size.height - 50,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                _appBar(context),
+                if (state == 0) ...[
+                  _body(context, emailController),
+                  _signup(context),
+                ] else ...[
+                  _fillOTP(context, emailController),
+                ]
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _appBar(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        CircleAvatar(
+          backgroundImage: const AssetImage('lib/assets/images/logo.png'),
+          radius: MediaQuery.of(context).size.height / 25,
+        ),
+        const Text(
+          'Travel Companion App',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ],
+    );
+  }
+
+  _body(context, TextEditingController email) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 10 * 7.5,
+      padding: EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 6,
+            child: Container(
+              alignment: Alignment.center,
+              child: const Text(
+                "Email Verification",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Container(
+            child: Column(children: [
+              Container(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Enter your email')),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 10,
+                child: TextField(
+                  controller: email,
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(18),
+                        borderSide: BorderSide.none),
+                    fillColor: Colors.amber.withOpacity(0.1),
+                    filled: true,
+                    // prefixIcon: const Icon(Icons.person)
+                  ),
+                ),
+              ),
+            ]),
+          ),
+          const SizedBox(height: 15),
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 12,
+            child: ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  state = 1;
+                });
+              },
+              style: ElevatedButton.styleFrom(
+                shape: const StadiumBorder(),
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                backgroundColor: Colors.amberAccent,
+              ),
+              child: const Text(
+                'Send OTP',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color.fromRGBO(97, 97, 97, 1),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _signup(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Already have an account? "),
+        TextButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (_) => LoginPage()));
+            },
+            child: const Text(
+              "Login",
+              style: TextStyle(color: Colors.amber),
+            ))
+      ],
+    );
+  }
+
+  _fillOTP(context, TextEditingController email) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 10 * 7.5,
+      padding: EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 6,
+            child: Container(
+              alignment: Alignment.center,
+              child: const Text(
+                "Email Verification",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Container(
+            child: Column(children: [
+              const Text('Enter the OTP send to you at'),
+              Text(
+                email.text,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                  alignment: Alignment.centerLeft,
+                  child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          state = 0;
+                        });
+                        email.clear();
+                      },
+                      child: const Text(
+                        "Change email",
+                        style: TextStyle(color: Colors.amber),
+                      ))),
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                  height: MediaQuery.of(context).size.height / 10,
+                  child: Pinput(
+                    length: 6,
+                    defaultPinTheme: PinTheme(
+                      width: MediaQuery.of(context).size.width / 8,
+                      height: MediaQuery.of(context).size.height / 10,
+                      textStyle: const TextStyle(
+                        fontSize: 20,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.amberAccent[100],
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    ),
+                    validator: (s) {
+                      return null;
+                    },
+                  )),
+            ]),
+          ),
+          const SizedBox(height: 15),
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 12,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (_) => SignupPage()));
+              },
+              style: ElevatedButton.styleFrom(
+                shape: const StadiumBorder(),
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                backgroundColor: Colors.amberAccent,
+              ),
+              child: const Text(
+                ' Next',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color.fromRGBO(97, 97, 97, 1),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/authentication/login.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+import 'package:travel_companiion/pages/authentication/email_verification.dart';
+import '../../main.dart';
+import '../../pages/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var emailController = TextEditingController();
+    var passwordController = TextEditingController();
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 251, 248, 239),
+        body: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.all(15),
+            height: MediaQuery.of(context).size.height - 50,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                _appBar(context),
+                _body(context, emailController, passwordController),
+                _signup(context),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _appBar(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        CircleAvatar(
+          backgroundImage: const AssetImage('lib/assets/images/logo.png'),
+          radius: MediaQuery.of(context).size.height / 25,
+        ),
+        const Text(
+          'Travel Companion App',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ],
+    );
+  }
+
+  _body(context, TextEditingController email, TextEditingController password) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 10 * 7.5,
+      padding: EdgeInsets.all(20),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 6,
+            child: Container(
+              alignment: Alignment.center,
+              child: const Text(
+                "LOGIN",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          Container(
+            child: Column(children: [
+              Container(alignment: Alignment.centerLeft, child: Text('Email')),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 10,
+                child: TextField(
+                  controller: email,
+                  decoration: InputDecoration(
+                    hintText: "Email",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(18),
+                        borderSide: BorderSide.none),
+                    fillColor: Colors.amber.withOpacity(0.1),
+                    filled: true,
+                    // prefixIcon: const Icon(Icons.person)
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('Password'),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Forgot password?",
+                      style: TextStyle(color: Colors.amber),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 10,
+                child: TextField(
+                  controller: password,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(18),
+                        borderSide: BorderSide.none),
+                    fillColor: Colors.amber.withOpacity(0.1),
+                    filled: true,
+                    // prefixIcon: const Icon(Icons.password),
+                  ),
+                  obscureText: true,
+                ),
+              ),
+            ]),
+          ),
+          const SizedBox(height: 15),
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 12,
+            child: ElevatedButton(
+              onPressed: () async {
+                try {
+                  final credential = await FirebaseAuth.instance
+                      .signInWithEmailAndPassword(
+                      email: email.text, password: password.text)
+                      .then((_) {
+                    Navigator.pushReplacement(
+                        context, MaterialPageRoute(builder: (_) => HomePage()));
+                  });
+                } on FirebaseAuthException catch (e) {
+                  if (e.code == 'user-not-found') {
+                    print('No user found for that email.');
+                  } else if (e.code == 'wrong-password') {
+                    print('Wrong password provided for that user.');
+                  }
+                }
+              },
+              style: ElevatedButton.styleFrom(
+                shape: const StadiumBorder(),
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                backgroundColor: Colors.amberAccent,
+              ),
+              child: const Text(
+                "Login",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Color.fromRGBO(97, 97, 97, 1),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  _signup(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const Text("Dont have an account? "),
+        TextButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (_) => VerifyPage()));
+            },
+            child: const Text(
+              "Sign Up",
+              style: TextStyle(color: Colors.amber),
+            ))
+      ],
+    );
+  }
+}
