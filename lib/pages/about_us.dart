1f17025fe44425631b62ba973695726461c2388a
Lakshya Jain <b23cs1032@iitj.ac.in>
2024-03-04 03:26:39 +0530

    Edit post debugged, Search result page debugged,contains added in search,snack bar added for req. fields in create post,changed the caption

--- lib/pages/about_us.dart
+++ lib/pages/about_us.dart
@@ -8,12 +8,21 @@ class about_us extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: secondaryColor,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back,color: secondaryTextColor,),
+          icon: const Icon(
+            Icons.arrow_back,
+            color: secondaryTextColor,
+          ),
           onPressed: () {
             Navigator.of(context).pop();
           },
         ),
-        title: Text('About Us',style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20,color: secondaryTextColor),),
+        title: Text(
+          'About Us',
+          style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 20,
+              color: secondaryTextColor),
+        ),
       ),
       backgroundColor: primaryColor,
       body: Center(
@@ -27,14 +36,32 @@ class about_us extends StatelessWidget {
                 fontSize: 20,
               ),
             ),
-            SizedBox(height: 8), 
-            Text('Anadi Sharma (Mentor)',style: TextStyle(fontWeight: FontWeight.bold),),
-            Text('Lakshya Jain',style: TextStyle(fontWeight: FontWeight.bold),),
-            Text('Divya Kumar',style: TextStyle(fontWeight: FontWeight.bold),),
-            Text('Radhika Agarwal',style: TextStyle(fontWeight: FontWeight.bold),),
-            Text('Arun Kumar',style: TextStyle(fontWeight: FontWeight.bold),),
-            Text('For any bugs please Contact Devlup Labs',style: TextStyle(fontWeight: FontWeight.bold),),
-
+            SizedBox(height: 8),
+            Text(
+              'Anadi Sharma (Mentor)',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Lakshya Jain',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Divya Kumar',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Radhika Agarwal',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'Arun Kumar',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text(" "),
+            Text(
+              'For any issues please Contact Devlup Labs',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
           ],
         ),
       ),
