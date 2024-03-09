4d5e881cc703ffa8fed0a3bfd53db8878348dc76
Arun Kumar <ak299730p@gmail.com>
2024-03-09 15:25:48 +0530

    Contact form added, Refresh functionality added to homepage

--- lib/pages/about_us.dart
+++ lib/pages/about_us.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:travel_companion/utils/colors.dart';
+import 'package:url_launcher/url_launcher.dart'; 
 
 class about_us extends StatelessWidget {
+
+  void _launchURL() async {
+    const url = 'https://forms.gle/JXBwR9KiEk83VwpD6';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +46,10 @@ class about_us extends StatelessWidget {
             Text('Radhika Agarwal',style: TextStyle(fontWeight: FontWeight.bold),),
             Text('Arun Kumar',style: TextStyle(fontWeight: FontWeight.bold),),
             Text('For any bugs please Contact Devlup Labs',style: TextStyle(fontWeight: FontWeight.bold),),
-
+            TextButton(  
+              onPressed: _launchURL,
+              child: Text('Contact Form'),
+            ),
           ],
         ),
       ),
